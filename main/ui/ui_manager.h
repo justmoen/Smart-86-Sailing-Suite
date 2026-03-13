8ac9e45d9d694f2952d56367910fea7bc96f879d
justinmoentrismax <jmoen@trismax.com>
2026-03-12 20:37:49 -0500

    update ui manager linkage

--- main/ui/ui_manager.h
+++ main/ui/ui_manager.h
@@ -1,14 +1,18 @@
-#pragma once
-
-#include "ui_screens.h"
+#ifndef UI_MANAGER_H
+#define UI_MANAGER_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include "ui_screens.h"
+
 void ui_manager_init(void);
 void ui_manager_update(void);
+void ui_next_screen(void);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
