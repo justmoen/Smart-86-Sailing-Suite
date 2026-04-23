266ad415d6b02e498dfbc01cde94d257b6d3e0c2
justinmoentrismax <jmoen@trismax.com>
2026-04-22 19:59:59 -0500

    corrections for display config ui screen selection

--- main/ui/navigation.h
+++ main/ui/navigation.h
@@ -12,8 +12,6 @@ extern int page;
 
 void default_settings(void);
 void gesture_event_cb(lv_event_t *e);
-void navigation_next_page(void);
-void navigation_prev_page(void);
 void save_brightness(int value);
 void save_last_screen(int index);
 int load_last_screen();
